95818d81ffa9e7c5912d2f53538642ba306bb5c2
Razgriz <enzerux@gmail.com>
2023-04-01 12:23:17 +0200

    init

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+tns({
+    container: '.slider__element',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    navPosition: 'bottom',
+    mouseDrag: true
+});
+
+let navigation = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+    // console.log(window.pageYOffset) - sprawdza odleglosc przy scrollowaniu
+    if (window.pageYOffset > 15) {
+        navigation.classList.add('navbar--move')
+    } else { navigation.classList.remove('navbar--move') }
+})
+
+let hamburger = document.querySelector('.navbar__hamburger')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('navbar__hamburger--active')
+    navigation.classList.toggle('navbar--open')
+})
+
+AOS.init()
